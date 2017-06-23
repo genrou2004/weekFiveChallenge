c185c484dd37eb0d2bee6947d4d3b2b9eb4eb907
student <student@GBTC440J10LX.montgomerycollege.edu>
2017-06-23 17:55:47 -0400

    modified

--- src/main/java/com/example/atm/Account.java
+++ src/main/java/com/example/atm/Account.java
@@ -20,6 +20,16 @@ public class Account {
     @Size(min=6, max=30)
     private String accountNumber;
     private String userName;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    private double amount;
     /*private String balance;
     private double amount;
     private String reason;*/
